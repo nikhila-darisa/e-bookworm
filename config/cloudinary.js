da514d5faed5ab600a563a70c212dfb48f61e41f
nikhiladarisa <nikhiladarisa@gmail.com>
2019-10-02 20:05:22 +0530

    final project

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,17 +1,11 @@
 
-// require('dotenv').config()
+require('dotenv').config()
 
-// var cloudinary = require('cloudinary').v2;
-// cloudinary.config({
-//     cloud_name: process.env.cloud_name,
-//     api_key: process.env.api_key,
-//     api_secret: process.env.api_secret
-// });
-// module.exports = cloudinary
 var cloudinary = require('cloudinary').v2;
 cloudinary.config({
-    cloud_name: 'ddr0gfrzp',
-    api_key: '334772227636851',
-    api_secret: '_LnTvipBDD-LZGx7liSAEvJwUp8'
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
 });
 module.exports = cloudinary
+
